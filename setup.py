9821d5378ca1022dd0ae8b2692895b1525116052
YuliaSheb <79199794+YuliaSheb@users.noreply.github.com>
2022-05-09 10:49:30 +0300

    LR2

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="serializer",
+    version="0.0.1",
+    author="Julia Shebeko",
+    description="Functionality for serialization/deserialization to different formats",
+    packages=['serializer'],
+    python_requires='>=3.8',
+)
